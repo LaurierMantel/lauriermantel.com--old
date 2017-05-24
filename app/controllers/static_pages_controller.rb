3538321146e673fde6f484b740272617326d97ad
Laurier Mantel <lauriermantel@gmail.com>
2017-05-24 14:33:53 -0400

    new resume

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
   def home
   end
   def download_resume
-  	send_file("#{Rails.root}/public/software-slash-data.pdf",
-              filename: "LAURIER MANTEL.pdf",
+  	send_file("#{Rails.root}/public/LaurierMantel.pdf",
+              filename: "LaurierMantel.pdf",
               type: "application/pdf")
   end
 end
